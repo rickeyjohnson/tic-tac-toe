399e70444f52701a935d3680a3c834a59ad6b5a0
rickeyjohnson <rdjwinter2004@gmail.com>
2024-08-21 14:48:02 -0400

    added display game board function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-function GameBoard() {
+function createGameBoard() {
     const board = [["", "", ""],
                    ["", "", ""],
                    ["", "", ""]]
 
     const addMark = (mark, row, column) => {
         board[row][column] = mark;
+    }
+
+    const displayGameBoard = () => {
+        console.log(board[0][0] + board[0][1] + board[0][2])
+        console.log(board[1][0] + board[1][1] + board[1][2])
+        console.log(board[2][0] + board[2][1] + board[2][2])
     }
 }
 
@@ -15,8 +21,9 @@ function createPlayer(name, mark) {
 function Game() {
     const player1 = createPlayer("player1", "X")
     const player2 = createPlayer("player2", "O")
+    const gameboard = createGameBoard()
 
     const startGame = () => {
-        // code
+        gameboard.displayGameBoard()
     }
 }
